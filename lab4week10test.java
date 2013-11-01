29c678016f54ef409d73a74e083667abebc9161d
ramirezcesar93 <ramirezicesar@gmaill.com>
2013-11-01 11:47:55 -0700

    lab4week10test

---
+++ lab4week10test.java
@@ -0,0 +1,14 @@
+
+package guess.a.number;
+
+/**
+ *
+ * @author user
+ */
+public class lab4week10test {
+    public static void main(String[] args){
+        lab4week10 testObject = new lab4week10();
+        testObject.OneThrow();
+    }
+    
+}
